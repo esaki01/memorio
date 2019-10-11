66997c3f17c0ffd4b0dad642a9d5030c10ce0d35
esaki01 <social0430@gmail.com>
2019-10-11 23:08:07 +0900

    Add .gitignore

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <div class="card">
+      <header class="card-header">
+        <div class="field has-addons search-form">
+          <div class="control">
+            <input class="input" type="text" placeholder="Find a word" />
+          </div>
+          <div class="control">
+            <a class="button">Search</a>
+          </div>
+        </div>
+      </header>
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+          <a
+            href="#"
+          >@bulmaio</a>.
+          <a href="#">#css</a>
+          <a href="#">#responsive</a>
+          <br />
+          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Save</a>
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  max-width: 600px;
+  margin: 50px auto 0 auto;
+}
+
+.search-form {
+  margin: 0 auto;
+  padding: 30px;
+}
+</style>>
