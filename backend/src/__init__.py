a2e407b5f746a1c165e1580e818f4d56d27d2c4a
esaki01 <social0430@gmail.com>
2019-10-13 00:28:01 +0900

    Add frontend url to cors

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -10,6 +10,6 @@ def create_app() -> Flask:
     app.register_blueprint(weblio)
 
     # TODO add production origin
-    CORS(app, origins=['http://localhost:8080/*'])
+    CORS(app, origins=['http://localhost:8080/*', 'https://frontend-o5x5u66yaq-uc.a.run.app'])
 
     return app
