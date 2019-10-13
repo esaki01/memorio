7c977e55aa5356b80309a63673949675b3740ae5
esaki01 <social0430@gmail.com>
2019-10-13 13:53:06 +0900

    Add terraform backend build

--- infra/src/build.tf
+++ infra/src/build.tf
@@ -16,3 +16,22 @@ resource "google_cloudbuild_trigger" "frontend-deploy" {
 
   filename = "frontend/cloudbuild.yaml"
 }
+
+resource "google_cloudbuild_trigger" "backend-deploy" {
+  description = "backend deploy"
+
+  trigger_template {
+    branch_name = "${lookup(var.build_trigger_branch, "${terraform.workspace}")}"
+    repo_name   = "github_esaki01_memorio"
+  }
+
+  included_files = [
+    "backend/**"
+  ]
+
+  substitutions = {
+    _GCP_PROJECT_ID = "${data.google_project.project.project_id}"
+  }
+
+  filename = "backend/cloudbuild.yaml"
+}
