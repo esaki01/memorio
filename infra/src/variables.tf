c0a7abb558bb11f331fd35ec64089496e95c7e16
esaki01 <social0430@gmail.com>
2019-10-19 03:51:12 +0900

    Add terraform compute engine

--- infra/src/variables.tf
+++ infra/src/variables.tf
@@ -1,5 +1,6 @@
 variable "project_number" {
   type = "map"
+
   default = {
     dev = "253807"
   }
@@ -7,6 +8,7 @@ variable "project_number" {
 
 variable "build_trigger_branch" {
   type = "map"
+
   default = {
     dev = "master"
   }
@@ -14,13 +16,23 @@ variable "build_trigger_branch" {
 
 variable "credential" {
   type = "map"
+
   default = {
     dev = "../config/credentials/dev.json"
   }
 }
 
+variable "mysql" {
+  type = "map"
+
+  default = {
+    password = "CiQAd+zGhZ5c+Y5DJEkZc+gxjueTdXWGB/csH4lIT4hoW/Mr6UESNADvaGnVFFwnXDBD/S9AscKTUGrEZJgUnZHxwfbdcYd4/txQRZKkIW/sJ9BDu0JXAe6W89w="
+  }
+}
+
 variable "service" {
   type = "list"
+
   default = [
     "cloudbuild.googleapis.com",
     "containerregistry.googleapis.com",
@@ -33,6 +45,8 @@ variable "service" {
     "pubsub.googleapis.com",
     "storage-api.googleapis.com",
     "identitytoolkit.googleapis.com",
-    "cloudkms.googleapis.com"
+    "cloudkms.googleapis.com",
+    "compute.googleapis.com",
+    "oslogin.googleapis.com",
   ]
 }
