833bc192191d5a6460d87c172c762a1ca8f30016
esaki01 <social0430@gmail.com>
2019-10-26 16:34:10 +0900

    Implement get pronunciations

--- backend/src/adapters/repositories/word_repository.py
+++ backend/src/adapters/repositories/word_repository.py
@@ -1,10 +1,10 @@
 from abc import ABCMeta, abstractmethod
 
 from src.domain.word import Word
-from typing import Optional
+from typing import Optional, List
 
 
 class WordRepository(metaclass=ABCMeta):
     @abstractmethod
-    def find_by_id(self, id: str) -> Optional[Word]:
+    def find_by_ids(self, ids: List[str]) -> List[Optional[Word]]:
         raise NotImplementedError()
--- backend/src/usecases/word/interactors/get_pronunciation_interactor.py
+++ backend/src/usecases/word/interactors/get_pronunciation_interactor.py
@@ -1,10 +1,11 @@
 import logging
-from typing import Optional
+from typing import Optional, List
 
 import requests
 from bs4 import BeautifulSoup
 
 from src.adapters.repositories.word_repository import WordRepository
+from src.domain.word import Word
 from src.usecases.word.dto.get_pronunciation_request import GetPronunciationRequest
 from src.usecases.word.dto.get_pronunciation_response import GetPronunciationResponse
 from src.usecases.word.get_pronunciation_usecase import GetPronunciationUseCase
@@ -17,21 +18,22 @@ class GetPronunciationInteractor(GetPronunciationUseCase):
         self._word_repo = word_repo
 
     def handle(self, request: GetPronunciationRequest) -> GetPronunciationResponse:
-        phonetic_symbol = self._get_pronunciation_from_repository(request.keyword)
+        keywords = [k.lower() for k in request.keyword.split()]
+        words = self._get_pronunciation_from_repository(keywords)
+        words_map = {w.id: w.phonetic_symbol for w in words if w}
 
-        if not phonetic_symbol:
-            phonetic_symbol = self._get_pronunciation_from_weblio(request.keyword)
+        phonetic_symbols = [words_map.get(k) for k in keywords]
 
-        return GetPronunciationResponse(phonetic_symbol)
+        # TODO Access weblio
+        # if not phonetic_symbol:
+        #     phonetic_symbol = self._get_pronunciation_from_weblio(k)
 
-    def _get_pronunciation_from_repository(self, keyword: str) -> Optional[str]:
-        word = self._word_repo.find_by_id(keyword)
+        return GetPronunciationResponse(phonetic_symbols)
 
-        if word:
-            return word.phonetic_symbol
-        else:
-            logging.info(f"Pronunciation of {keyword} is not found.")
-            return None
+    def _get_pronunciation_from_repository(
+        self, keywords: List[str]
+    ) -> List[Optional[Word]]:
+        return self._word_repo.find_by_ids(keywords)
 
     def _get_pronunciation_from_weblio(self, keyword: str) -> Optional[str]:
         response = requests.get(f"{self._WEBLIO_SITE_URL}/content/{keyword}")
