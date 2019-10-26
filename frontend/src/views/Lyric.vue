edc37b59b99cf7a12a4c6c04b4f38f2ffe0a534d
esaki01 <social0430@gmail.com>
2019-10-26 18:29:21 +0900

    Add lyric page

--- frontend/src/views/Lyric.vue
+++ frontend/src/views/Lyric.vue
@@ -7,18 +7,62 @@
         <p class="artist">{{ $route.params.artist }}</p>
       </div>
       <div class="column is-three-quarters">
-        <p>{{ $route.params.lyric }}</p>
+        <table class="table is-striped is-fullwidth">
+          <div v-for="i in 10" :key="i.id">
+            <tbody>
+              <tr>
+                <td v-for="l in 10" :key="l.id">
+                  <span class="title is-6">{{ lyrics[l - 1 + (i - 1) * 10] }}</span>
+                </td>
+              </tr>
+              <tr>
+                <td v-for="p in 10" :key="p.id">
+                  <span class="subtitle is-6">{{ pronunciations[p - 1 + (i - 1) * 10] }}</span>
+                </td>
+              </tr>
+            </tbody>
+          </div>
+        </table>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function() {
     return {
-
+      lyrics: [],
+      pronunciations: []
     };
+  },
+  created: function() {
+    this.get_pronunciation(this.$route.params.lyric);
+    this.lyrics = this.$route.params.lyric.split(/\s+/);
+  },
+  methods: {
+    get_pronunciation: function(k) {
+      axios
+        .get(
+          "https://backend-n2zzz72gsq-uc.a.run.app/word/pronunciation/search",
+          {
+            params: { keyword: k }
+          }
+        )
+        .then(response => {
+          if (response.data.data) {
+            this.pronunciations = response.data.data.phonetic_symbols;
+          } else {
+            console.log("Not data.");
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        })
+        .finally(() => {});
+    }
   }
 };
 </script>
